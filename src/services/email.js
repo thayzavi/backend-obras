819a73524420a34106e4d98b9224b2517b762ed3
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-06-21 11:39:25 -0300

    Update email.js
    add fiscalizações

--- src/services/email.js
+++ src/services/email.js
@@ -11,7 +11,27 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-const sendObraDetailsEmail = async(toEmail, obra) => {
+const sendObraDetailsEmail = async(toEmail, obra, fiscalizacoes = []) => {
+    let fiscalizacoesHtml = '';
+
+    if(fiscalizacoes.length > 0){
+        fiscalizacoesHtml = `
+        <h2>Fiscalizações</h2>
+        <ul>
+        ${fiscalizacoes.map( f => `
+            <li style="margin-bottom: 15px;">
+            <p><strong>Data:</strong> ${new Date(f.data).toLocaleDateString()}</p>
+            <p><strong>Status:</strong> ${f.status}</p>
+            <p><strong>Observações:</strong> ${f.observacoes || 'Nenhuma'}</p>
+            <p><strong>Endereço:</strong> ${f.endereco}</p>
+            ${f.foto ? `<img src="${f.foto}" width="200" style="margin-top:5px;" />` : ''}
+            </li>
+            `).join('')}
+        </ul>
+        `;
+    } else {
+        fiscalizacoesHtml = '<p><em>Nenhuma fiscalização registrada.</em></p>';
+    }
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: toEmail,
@@ -23,7 +43,10 @@ const sendObraDetailsEmail = async(toEmail, obra) => {
         <p><strong> Data de Início:</strong> ${obra.dataInicio.toLocaleDateString()}</p>
         <p><strong>Localização:${obra.endereco}</p>
         <p><strong>Descrição:</strong> ${obra.descricao || 'Nenhuma descrição fornecida'}</p>
-        ${obra.foto ? `<img src="${obra.foto}" alt="Foto da Obra" width="300">` : ''}`
+        ${obra.foto ? `<img src="${obra.foto}" alt="Foto da Obra" width="300">` : ''}
+        <hr />
+        ${fiscalizacoesHtml}
+        `
     };
 
     try{
