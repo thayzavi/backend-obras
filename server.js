7956ca4a5a7f612fa84e8fdc4e8ad0788ebf4182
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-06-21 23:43:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
 require('dotenv').config();
 
 connectDB();
