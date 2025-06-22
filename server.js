72085204b85b54b69e36cc0680a3d312dc0b1ae9
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-06-21 22:47:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
